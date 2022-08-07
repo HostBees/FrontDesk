d9ff1b497f0552133b67d1f1c28e127c40a34309
Harsh-14 <harshhp948@gmail.com>
2022-08-08 01:20:57 +0530

    adding gif

--- src/App.js
+++ src/App.js
@@ -1,18 +1,24 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Lottie from "react-lottie";
+import animationData from './lotties/19877-the-beest-sticker-1.json';
 function App() {
+  const defaultOptions = {
+    loop: true,
+    autoplay: true,
+    animationData: animationData,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice",
+    },
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-         HostBees Welcomes You !
-        </p>
-        <h6>
-         admin@hostbees.me
-        </h6>
-       
+        {/* <img src={logo} className="App-logo" alt="logo" /> */}
+        <Lottie options={defaultOptions} height={400} width={400} />
+        <p>HostBees Welcomes You !</p>
+        <h6>admin@hostbees.me</h6>
       </header>
     </div>
   );
